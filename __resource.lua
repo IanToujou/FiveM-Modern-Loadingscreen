abe7c9812d28fb63bef274c1252f9f8696e94648
IanToujou <ianbour2016@gmail.com>
2020-11-15 19:21:29 +0100

    Initial commit

---
+++ __resource.lua
@@ -0,0 +1,18 @@
+files {
+    'index.html',
+	'styles.css',
+	'assets/css/Features-Clean.css',
+	'assets/fonts/font-awesome.min.css',
+	'assets/fonts/FontAwesome.otf',
+	'assets/fonts/fontawesome-webfont.eot',
+	'assets/fonts/fontawesome-webfont.woff',
+	'assets/fonts/fontawesome-webfont.woff2',
+	'assets/js/jquery.min.js',
+	'assets/bootstrap/css/bootstrap.min.css',
+	'assets/bootstrap/js/bootstrap.min.js',
+	'assets/img/background.gif'
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
